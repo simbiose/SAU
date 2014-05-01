67df04b64440574554c6cdf15250fb6d0a70bd09
Ademar Alves de Oliveira <ademar111190@gmail.com>
2014-05-01 14:56:26 -0300

    correct type and name

--- sau/src/main/java/simbio/se/sau/model/location/AbstractGeoLocationModel.java
+++ sau/src/main/java/simbio/se/sau/model/location/AbstractGeoLocationModel.java
@@ -17,13 +17,11 @@
  */
 package simbio.se.sau.model.location;
 
-import simbio.se.sau.exceptions.location.AbstractLocationException;
+import simbio.se.sau.model.IAbstractModel;
 
 /**
  * Created by Ademar Oliveira <ademar111190@gmail.com> on 4/30/14.
  */
-public interface GeoLocationModel {
-
-    public void couldNotGetGeoLocation(AbstractLocationException because);
+public abstract class AbstractGeoLocationModel implements IAbstractModel {
 
 }
